131cd0a2c45c7bbdea0fb1c371169470fd0d3a33
Francesco Paolocci (Micky5991) <micky5991@gmail.com>
2017-10-18 17:46:45 +0200

    Increased Versionnumber to 1.3.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VehicleInfo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This library utilizes the Micky5991/GT-MP-vehicleInfo and provides a simple way to get information about vehicles in GTA V")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VehicleInfo")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
